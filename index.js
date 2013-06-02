07416dd7f9f60e590adae1a6bf1fd4e64a8ba0f9
Max Olson <maxprogram@gmail.com>
2013-06-02 10:17:26 -0600

    Fix useORM bug; include ejs package

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ var path = require('path'),
     fs = require('fs');
 
 var log = require('./lib/log'),
-    gcj = require('grand-central-junction'),
-    ORM = require('./lib/orm');
+    gcj = require('grand-central-junction');
 
 exports.App = GrandCentral;
-exports.Model = ORM.prototype.Model;
 
-function GrandCentral(app, dir) {
+
+function GrandCentral(app, dir, useORM) {
     if (!app) log.error("Express app not found");
     if (!dir) log.error("Project directory not found");
 
+    this.useORM = useORM || true;
     this.app = app;
     this.dir = dir + '/';
     this.env = process.env.NODE_ENV || 'development';
     this.time = new Date();
-    this.orm = new ORM();
 
     var self = this;
     app.getModels = function(cb) {
-        if (self.useORM) return self.orm.getModels(cb);
-        else return cb("ORM is not being used");
+        if (self.useORM) {
+            var ORM = require('./lib/orm');
+            return new ORM().getModels(cb);
+        } else return cb("ORM is not being used");
     };
+
+    exports.Model = (self.useORM) ?
+        require('./lib/orm').prototype.Model :
+        function(model) {
+            return { name: model.name };
+        };
 }
 
 var fn = GrandCentral.prototype;
@@ -32,13 +39,15 @@ fn.route = function(options) {
     options = options || {};
     var self = this;
 
-    if (this.useORM) this.orm.getModels(function(models) {
-        return gcj.route(self.app, {
-            dir: self.dir,
-            models: models
+    if (this.useORM) {
+        var ORM = require('./lib/orm');
+        new ORM().getModels(function(models) {
+            return gcj.route(self.app, {
+                dir: self.dir,
+                models: models
+            });
         });
-    });
-    else return gcj.route(self.app, { dir: self.dir });
+    } else return gcj.route(self.app, { dir: self.dir });
 };
 
 fn.pipeline = function(options) {
@@ -56,5 +65,7 @@ fn.pipeline = function(options) {
 
     return require('grand-central-pipeline')(ops);
 };
+
+
 
 fn.uglify = fn.pipeline;
